9ab84bc7194026bba58dc310dd6a041d165bd6b9
Raúl <rsotoalbors@gmail.com>
2017-06-20 19:09:51 -0400

    Revert "Update code"
    This reverts commit 808fc5553aa817aeda73530541ef385fe44a4d47.

--- terminalGraphsTest.m
+++ terminalGraphsTest.m
@@ -1,49 +1,25 @@
+%dataDir = dir('C:\vr\vroutput\*.csv');
 
-%UNCOMMENT NEXT LINE AFTER TESTS ARE DONE
-%sessionData = getLatestFile('C:\vr\vroutput\*.csv');
-sessionData = ('MTH3_vr1_2017615_1731.csv');
-filePath = fullfile('C:', 'vr', 'vroutput', sessionData);
+sessionData = getLatestFile('/Users/Raul/*.csv');
+filePath = fullfile('Users', 'Raul', sessionData);
 
 fileID = fopen(filePath, 'r');
 
-%read .csv with session data
+% read .csv with session data
 data = textscan(fileID, '%f %f %f %f %f %f %f %f %f', 'Delimiter', ';');
 
-%breakdown of .csv contents
-t = [data{1}];
-pos = [data{2}];
-tFrame = [data{3}];
-vel = [data{4}];
-vrWorld = [data{5}];
-valveStat = [data{6}];
-numTrials = [data{7}];
-numLicks = [data{8}];
-wheelVel = [data{9}];
-
-lick = find(numLicks);      % indexes of licks
-noLick = find(~numLicks);   % indexes of no licks
-shortTrial = find(vrWorld == 3);    % indexes of short trials
-longTrial = find(vrWorld == 4);     % indexes of long trials
-blackbox = find(vrWorld == 5);      % indexes of black boxes
-
-totalShortTrials = size(unique(numTrials(shortTrial)),1);
-totalLongTrials = size(unique(numTrials(longTrial)),1);
+pos = [data{2}]
+numTrials = [data{7}]
+numlicks = [data{8}]
 
 figure(1)
-for line = data(lick)
-    if 
-    plot(pos(lick), numTrials(lick), 'o')
-    xlabel('Position')
-    ylabel('Trial #')
-    title('Short trials')
+for trial = 1:numTrials
+ plot(pos,trial,'+')
+ hold on
 end
+xlabel('distance (cm)')
+ylabel('trial number')
+hold off
 
-figure(2)
-if vrWorld(longTrial)
-    plot(pos(lick), numTrials(lick), 'o')
-    xlabel('Position')
-    ylabel('Trial #')
-    title('Long trials')
-end
 
 fclose(fileID);
