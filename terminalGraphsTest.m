4af8dda08d1e479058b08a68bec0e82c43349d8b
Raúl <rsotoalbors@gmail.com>
2017-06-21 19:21:44 -0400

    Update terminalGraphsTest.m

--- terminalGraphsTest.m
+++ terminalGraphsTest.m
@@ -1,71 +1,105 @@
 %UNCOMMENT NEXT LINE AFTER TESTS ARE DONE 
 %sessionData = getLatestFile('C:\vr\vroutput\*.csv'); 
-
- 
-
 sessionData = ('MTH3_vr1_2017615_1731.csv'); 
-%filePath = fullfile('C:', 'vr', 'vroutput', sessionData); 
-filePath = fullfile('Users', 'Raul', 'coding', 'mit', 'harnett', 'msrp-matlab', sessionData)
- 
-fileID = fopen(filePath, 'r'); 
+filePath = fullfile('C:', 'vr', 'vroutput', sessionData); 
+file = fopen(filePath, 'r'); 
+% read .csv with session data 
+data = textscan(file, '%f %f %f %f %f %f %f %f %f', 'Delimiter', ';'); 
 
-%read .csv with session data 
-data = textscan(fileID, '%f %f %f %f %f %f %f %f %f', 'Delimiter', ';'); 
+% breakdown of .csv contents 
+t = [data{1}];              % Time
+pos = [data{2}];            % Position
+tFrame = [data{3}];         % Time since last frame
+vel = [data{4}];            % Velocity
+vrWorld = [data{5}];        % Current trial (short, long, blackbox)
+valveStat = [data{6}];      % Valve status
+numTrials = [data{7}];      % Number of trials
+numLicks = [data{8}];       % Number of licks
+wheelVel = [data{9}];       % Wheel velocity
 
-%breakdown of .csv contents 
-t = [data{1}]; 
-pos = [data{2}]; 
-tFrame = [data{3}]; 
-vel = [data{4}]; 
-vrWorld = [data{5}]; 
-valveStat = [data{6}]; 
-numTrials = [data{7}]; 
-numLicks = [data{8}]; 
-wheelVel = [data{9}];
+% Join individual cell arrays into a single matrix
+allData = [t pos tFrame vel vrWorld valveStat numTrials numLicks wheelVel];
 
-S = vrWorld == 3;
+% Logicals. S for short, L for long.
+S = vrWorld == 3; 
 L = vrWorld == 4;
 
+% Mouse position for each trial
 posShort = pos(S);
 posLong = pos(L);
 
+% Trial # for each type of trial
 numTrialsShort = numTrials(S);
 numTrialsLong = numTrials(L);
 
+% Licks on each trial
 numLicksShort = numLicks(S);
 numLicksLong = numLicks(L);
- 
-licksShort = find(numLicksShort);      % indexes of licks 
+
+% Indexes of licks on each trial
+licksShort = find(numLicksShort);
 licksLong = find(numLicksLong);
 
-% noLick = find(~numLicks);   % indexes of no licks 
-% shortTrial = find(vrWorld == 3);    % indexes of short trials 
-% longTrial = find(vrWorld == 4);     % indexes of long trials 
-% blackbox = find(vrWorld == 5);      % indexes of black boxes
+% Total trials
+totalShortTrials = length(unique(numTrialsShort)); 
+totalLongTrials = length(unique(numTrialsLong)); 
+
+% Calculate total distance in short trials
+temp = zeros(length(posShort),1);
+for t = 1:length(temp)-1
+    if numTrials(t + 1) ~= numTrials(t)
+        temp(t) = 1;
+    end
+end 
+
+shortDistTemp = find(temp .* posShort); % Indices of distance travelled on each short trial
+distShort = pos(shortDistTemp);         % Returns distance travelled
+totDistShort = sum(distShort);
+
+% Calculate total distance in long trials
+temp = zeros(length(posLong),1);
+for t = 1:length(temp)-1
+    if numTrials(t + 1) ~= numTrials(t);
+        temp(t) = 1;
+    end
+end
+
+longDistTemp = find(temp .* posLong); % Indices of distance travelled on each long trial
+distLong = pos(longDistTemp);         % Returns distance travelled
+totDistLong = sum(distLong);
  
-% totalShortTrials = size(unique(numTrials(shortTrial)),1); 
-% totalLongTrials = size(unique(numTrials(longTrial)),1); 
- 
-figure(1) 
-    plot(posShort(licksShort), numTrialsShort(licksShort), 'o') 
+figure
+    subplot(6,5,[2 28])
+    plot(posShort(licksShort), numTrialsShort(licksShort), 'bo') 
     xlabel('Position') 
     ylabel('Trial #') 
     title('Short trials') 
+    hold on;
     
-figure(2)
-    plot(posLong(licksLong), numTrialsLong(licksLong), 'o')
+    subplot(6,5,[4 30])
+    plot(posLong(licksLong), numTrialsLong(licksLong), 'ro')
     xlabel('Position')
     ylabel('Trial #')
     title('Long trials')
-%  
-% figure(2) 
-% if vrWorld(longTrial) 
-%     plot(pos(lick), numTrials(lick), 'o') 
-%     xlabel('Position') 
-%     ylabel('Trial #') 
-%     title('Long trials') 
-% end 
+    hold on;
+    
+    ax1 = axes('Position', [0.01 0 1 1], 'Visible', 'off');
+    descr = {'Short trials:';
+        strcat(num2str(totalShortTrials), ' trials');
+        strcat(num2str(totDistShort), ' cm');
+        '';
+        'Long trials:';
+        strcat(num2str(totalLongTrials), ' trials');
+        strcat(num2str(totDistLong), ' cm')};
+    text(0.025,0.5,descr)
+    
+    x0 = 350;
+    y0 = 200;
+    width = 700;
+    height = 400;
+    set(gcf, 'units','points','position',[x0,y0,width,height])
 
+saveSession = [sessionData '.fig']
+savefig(saveSession)
 
-
-fclose(fileID); 
+fclose(file); 
