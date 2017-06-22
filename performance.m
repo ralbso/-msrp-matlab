028420e6337992de7a66a20a1d68e4b0243352c3
Raúl <rsotoalbors@gmail.com>
2017-06-22 19:03:14 -0400

    Update performance.m

--- performance.m
+++ performance.m
@@ -1,4 +1,4 @@
-function performance(bin)
+%function performance(bin)
 % PERFORMANCE Quick glance at behavior during session.
 %   Function takes either 0 or 1 as arg.
 %   0 for latest session performance overview; 1 to select a session.
@@ -8,23 +8,25 @@ function performance(bin)
 % tic
 
 % Select latest .csv in directory
-if bin == 0
-    file_name = getLatestFile('C:\vr\vroutput\*.csv'); 
-    path = 'C:\vr\vroutput\';
-end
+% if bin == 0
+%     file_name = getLatestFile('C:\vr\vroutput\*.csv'); 
+%     path = 'C:\vr\vroutput\';
+% end
+% 
+% % Pop-up box for .csv file selection
+% if bin == 1
+%     [file_name, path] = uigetfile('*.csv', 'Select raw data', 'C:\vr\vroutput\');
+%     if isequal(file_name, 0)
+%         error('File selection was cancelled.')
+%     end
+% end
+% 
+% % Arg can only be 0 or 1
+% if bin > 1
+%     error('Arg can only be 0 or 1.')
+% end
 
-% Pop-up box for .csv file selection
-if bin == 1
-    [file_name, path] = uigetfile('*.csv', 'Select raw data', 'C:\vr\vroutput\');
-    if isequal(file_name, 0)
-        error('File selection was cancelled.')
-    end
-end
-
-% Arg can only be 0 or 1
-if bin > 1
-    error('Arg can only be 0 or 1.')
-end
+[file_name, path] = uigetfile('*.csv', 'Select raw data', 'C:\vr\vroutput\');
 
 file_path = fullfile(path, file_name); 
 raw_data = fopen(file_path, 'r'); 
@@ -33,18 +35,18 @@ raw_data = fopen(file_path, 'r');
 data = textscan(raw_data, '%f %f %f %f %f %f %f %f %f', 'Delimiter', ';'); 
 
 % breakdown of .csv contents 
-t = [data{1}];              % Time
-pos = [data{2}];            % Position
-t_frame = [data{3}];         % Time since last frame
-vel = [data{4}];            % Velocity
-vr_world = [data{5}];        % Current trial (short, long, blackbox)
-valve_stat = [data{6}];      % Valve status
-num_trials = [data{7}];      % Number of trials
-num_licks = [data{8}];       % Number of licks
-wheel_vel = [data{9}];       % Wheel velocity
+t = [data{1}];                  % Time
+pos = [data{2}];                % Position
+t_frame = [data{3}];            % Time since last frame
+vel = [data{4}];                % Velocity
+vr_world = [data{5}];           % Current trial (short, long, blackbox)
+valve_stat = [data{6}];         % Valve status
+num_trials = [data{7}];         % Number of trials
+licks = [data{8}];              % Number of licks
+wheel_vel = [data{9}];          % Wheel velocity
 
 % Create matrix from individual cell arrays (for easier handling)
-all_data = [t pos t_frame vel vr_world valve_stat num_trials num_licks wheel_vel];
+all_data = [t pos t_frame vel vr_world valve_stat num_trials licks wheel_vel];
 
 % Logicals. S for short, L for long.
 S = vr_world == 3; 
@@ -59,39 +61,45 @@ num_trials_short = num_trials(S);
 num_trials_long = num_trials(L);
 
 % Licks on each trial
-num_licks_short = num_licks(S);
-num_licks_long = num_licks(L);
+licksS = licks(S);
+licksL = licks(L);
 
 % Indexes of licks on each trial 
-licks_short = find(num_licks_short); 
-licks_long = find(num_licks_long); 
-
-valve_stat_short = valve_stat(S);
-valve_stat_long = valve_stat(L);
+licksS = find(licksS); 
+licksL = find(licksL); 
 
 % Total trials
 total_short_trials = length(unique(num_trials_short)); 
 total_long_trials = length(unique(num_trials_long)); 
 
-% Kudos to Quique, who helped me figure out for loops in MATLAB
+% Honorable mention to Quique, who helped me refresh for loops in MATLAB
 
 % Separate arrays for triggered vs. default reward
-tempT = zeros(length(valve_stat),1);
-for t = 1:length(tempT)-1
+tempS = zeros(length(valve_stat),1);
+for t = 1:length(tempS)-1
     if valve_stat(t) ~= valve_stat(t+1) && valve_stat(t) == 1  % Triggered reward
-        tempT(t) = 1; 
+        tempS(t) = 1; 
     end
 end
 
-triggered_rew = find(tempT);       % Indices of triggered rewards
-find(num_licks(triggered_rew));    % Indices of licks && triggered
+spout_open = find(tempS);       % Indices of triggered rewards
+find(licks(spout_open));        % Indices of licks && triggered
+
+tempR = zeros(length(valve_stat),1);
+for t = 1:length(tempR)-1
+    if valve_stat(t) == 1
+        tempR(t) = 1;
+    end
+end
+
+rewards = find(tempR);      % Indices of rewards
 
 % Average velocity of mouse
 tempV = zeros(length(vel),1);
 thresh = 0.7;
 for t = 1:length(vel)-1
     if vel(t) > thresh
-        tempV(t) = vel(t); % velocities under thresh will be 0
+        tempV(t) = vel(t);  % Velocities under thresh will be 0
     end
 end
 
@@ -103,7 +111,7 @@ figure
     
     % Short trials
     subplot(6,5,[2 28])
-    plot(pos_short(licks_short), num_trials_short(licks_short), 'bo') 
+    plot(pos_short(licksS), num_trials_short(licksS), 'bo') 
     
     xlabel('Location (cm)') 
     xlim([50 350])
@@ -122,7 +130,7 @@ figure
     
     % Long trials
     subplot(6,5,[4 30])
-    plot(pos_long(licks_long), num_trials_long(licks_long), 'ro')
+    plot(pos_long(licksS), num_trials_long(licksS), 'ro')
     
     xlabel('Location (cm)')
     xlim([50 420])
@@ -157,10 +165,10 @@ figure
     height = 500;   
     set(gcf, 'units','points','position',[x0,y0,width,height])
 
-file_name = strrep(file_name, '.csv', '');
-save_session = [file_name '.png'];
-saveas(gcf, save_session);
+% file_name = strrep(file_name, '.csv', '');
+% save_session = [file_name '.png'];
+% saveas(gcf, save_session);
 
 fclose(raw_data); 
 % toc
-end
+%end
